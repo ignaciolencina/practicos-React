12b980fef4f6fe88ac1ed68c4d75e3db508d19f5
Ignacio Lencina <nacholencina60@gmail.com>
2024-06-12 23:22:57 -0300

    Completo ejercicio 4

---
+++ src/components/Ejercicio4/Ejercicio4.jsx
@@ -0,0 +1,52 @@
+import { useRef, useState } from "react";
+import "./Ejercicio4.css";
+
+const Ejercicio4 = () => {
+  const [toDos, setToDos] = useState([]);
+
+  const $input = useRef();
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      const newToDo = $input.current.value;
+      if (newToDo.trim()) {
+        const newArray = [...toDos, newToDo];
+        setToDos(newArray);
+        $input.current.value = "";
+      }
+    }
+  };
+
+  return (
+    <section className="container">
+      <section className="card mt-5">
+        <article className="text-center">
+          <h1>Bienvenido</h1>
+          <h3 className="fs-5">Ingresa tus tareas</h3>
+        </article>
+        <article className="text-center my-2">
+          <fieldset>
+            <label></label>
+            <input
+              type="text"
+              ref={$input}
+              onKeyDown={handleEnter}
+              className="input-toDo px-2"
+              placeholder="Tarea... 'Enter'"
+            />
+          </fieldset>
+          <ul className="lista-toDos mt-4">
+            {toDos.map((toDo, index) => {
+              return (
+                <li key={index} className="toDos">
+                  {toDo}
+                </li>
+              );
+            })}
+          </ul>
+        </article>
+      </section>
+    </section>
+  );
+};
+export default Ejercicio4;
