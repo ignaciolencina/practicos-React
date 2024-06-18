575f3647bd02109422755430226f936ff52f600c
Ignacio Lencina <nacholencina60@gmail.com>
2024-06-18 12:49:45 -0300

    Borro el estilo del app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 // import Ejercicio1 from "./components/Ejercicio1/Ejercicio1";
 // import Ejercicio2 from "./components/Ejercicio2/Ejercicio2";
 // import Ejercicio3 from "./components/Ejercicio3/Ejercicio3";
